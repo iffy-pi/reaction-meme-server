c122e08e125b3fd165db248ecd046b2879cdd1f0
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-12-04 05:29:47 -0500

    Implemented simple search with tags and name

---
+++ memeSearch.py
@@ -0,0 +1,52 @@
+import json
+import sys
+import os
+
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
+from whoosh import index
+from whoosh.qparser import QueryParser, OrGroup, MultifieldParser
+def main():
+    # create our schema, fields in the document we are searching or storing per search
+    schema = Schema(fileID=STORED,
+                    name=TEXT(stored=True, analyzer=StemmingAnalyzer()),
+                    tags=KEYWORD(lowercase=True, scorable=True, commas=True, analyzer=StemmingAnalyzer()))
+
+    # create our index, maps the searchable fields to actual documents, in this case our json element IDs
+    if not os.path.exists("indexdir"):
+        os.mkdir("indexdir")
+
+    # if index.exists_in("indexdir"):
+    #     ix = index.open_dir("indexdir")
+    # else:
+    ix = index.create_in("indexdir", schema)
+
+    with open('preDB.json', 'r') as file:
+        db = json.load(file)
+
+    # Add documents to the schema
+    writer = ix.writer()
+
+    for elem in db['elements'].values():
+        # print('Adding element:')
+        # print(elem)
+        writer.add_document(fileID=elem['id'], name=elem['name'], tags=','.join(elem['tags']))
+
+    writer.commit()
+
+    qp = MultifieldParser(["tags", "name"], schema=ix.schema, group=OrGroup)
+    q = qp.parse(u"human torture")
+
+    with ix.searcher() as s:
+        results = s.search(q)
+        for r in results:
+            print(r)
+
+
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
