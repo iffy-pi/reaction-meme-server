b9bc8022f20e9025c52790059765bd7986a841c3
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-12-26 20:38:34 -0500

    Updated with general help command

---
+++ apiutils/generalUtils.py
@@ -0,0 +1,15 @@
+from apiutils.FileStorageClasses.PBFSFileStorage import PBFSFileStorage
+from apiutils.FileStorageClasses.RepoLocalFileStorage import RepoLocalFileStorage
+from apiutils.configs.ServerConfig import ServerConfig
+
+
+def saveLocalJSONDBIntoPBFS():
+    """
+    Saves data/db.json into PBFS so it matches
+    """
+    pbfs = PBFSFileStorage(ServerConfig.PBFS_ACCESS_TOKEN, ServerConfig.PBFS_SERVER_IDENTIFIER)
+    lcfs = RepoLocalFileStorage()
+    db = lcfs.getJSONDB()
+    print('Writing data/db.json')
+    pbfs.writeJSONDB(db)
+    print('Done')
